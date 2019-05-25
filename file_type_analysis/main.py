50a233bc3b699f2361ceb5952db7ae8c362ce881
Владислав Плоткин <vlad@krasnodar-it-service.ru>
2019-05-26 01:39:51 +0300

    Fixed bugs in the program

--- file_type_analysis/main.py
+++ file_type_analysis/main.py
@@ -22,27 +22,23 @@ class analysis_file():
 
     def read_extensions(self):
         a = dict()
-        max = self.count_lines(self.indexfile)
+        maxstring = self.count_lines(self.indexfile)
         with open(self.indexfile,'r') as indefile:
-            count = 0
+            count_string = 0
             for line in indefile:
-
                 string = line.split(";")
-                try:
-                    ext = re.match(r'.*\.(.*)\"$', string[self.unpack_map['orig']]).groups(1)[0]
-                    path = '/'.join([self.dir] + [string[i] for i in self.unpack_map['path']] + \
-                                    [string[self.unpack_map['filename']].replace('"','')+'.'+self.type_file])
-                    if os.path.isfile(path):
-                        if not ext in a:
-                            a[ext] = 0
-                        else:
-                            a[ext] += os.path.getsize(path)
-                    self.progress(count, max)
-                    count += 1
-                except:
-                    pass
-
-
+                ext = re.match(r'.*\.(.*)$', string[self.unpack_map['orig']].decode('utf-8'),re.I | re.U)
+                if not ext:
+                    continue
+                ext = ext.group(1).lower()
+                path = '/'.join([self.dir] + [string[i].replace('"','') for i in self.unpack_map['path']] + [string[self.unpack_map['filename']].replace('"','')+'.'+self.type_file])
+                if os.path.isfile(path):
+                    if not ext in a:
+                        a[ext] = 0
+                    else:
+                        a[ext] += os.path.getsize(path)
+                self.progress(count_string, maxstring)
+                count_string += 1
         return a
     def __construct_filename__(self):
         path = sorted([ i for i in range(len(self.map)) if 'path' in self.map[i] ])
@@ -62,18 +58,17 @@ class analysis_file():
     def count_lines(self, filename, chunk_size=1 << 13):
         with open(filename) as file:
             return sum(chunk.count('\n') for chunk in iter(lambda: file.read(chunk_size), ''))
-    def progress(self,i,max):
-        i = int(100/max * i)
+    def progress(self,i,maxfile):
+        persent = int( 100/float(maxfile)*i)
         sys.stdout.write('\r')
         # the exact output you're looking for:
-        sys.stdout.write("[%-100s] %d%%" % ('=' * i, i))
-        if i >= 90:
-            sys.stdout.write('\n')
+        sys.stdout.write("[%-100s] %d%%" % ('=' * persent, persent))
         sys.stdout.flush()
 
 if __name__ == "__main__":
     a = analysis_file(os.path.join(os.path.dirname(__file__),'config.yaml'))
     for i in a:
         extension = a.read_extensions()
+        print("\nResult:")
         for ext in extension:
             print("%s: %s"%(ext,a.convert_bytes(extension[ext])))
